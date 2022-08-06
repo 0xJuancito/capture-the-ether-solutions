81a8cc83b41ccbbda05f9e450bb08cabbc6b538d
juancito.eth <juancitoeth@gmail.com>
2022-08-06 15:07:12 -0300

    format files with prettier

--- tasks/call-me.ts
+++ tasks/call-me.ts
@@ -1,9 +1,9 @@
 import { task } from "hardhat/config";
 
-const contractAddress = '0xdF5c182084B7dBdcca5257DC5cd829Cb14970B6a';
+const contractAddress = "0xdF5c182084B7dBdcca5257DC5cd829Cb14970B6a";
 
 task("call-me", "Solves the 'Call Me' challenge", async (_taskArgs, hre) => {
-  const factory = await hre.ethers.getContractFactory('CallMeChallenge');
+  const factory = await hre.ethers.getContractFactory("CallMeChallenge");
   const contract = factory.attach(contractAddress);
 
   const tx = await contract.callme();
--- tasks/choose-a-nickname.ts
+++ tasks/choose-a-nickname.ts
@@ -1,12 +1,12 @@
 import { task } from "hardhat/config";
 
-const contractAddress = '0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee';
+const contractAddress = "0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee";
 
-task("choose-a-nickname", "Solves the 'Call Me' challenge", async (taskArgs: {nickname: string}, hre) => {
-  const factory = await hre.ethers.getContractFactory('CaptureTheEther');
+task("choose-a-nickname", "Solves the 'Choose a Nickname' challenge", async (taskArgs: { nickname: string }, hre) => {
+  const factory = await hre.ethers.getContractFactory("CaptureTheEther");
   const contract = factory.attach(contractAddress);
 
-  const nickname = hre.ethers.utils.formatBytes32String(taskArgs.nickname)
+  const nickname = hre.ethers.utils.formatBytes32String(taskArgs.nickname);
   const tx = await contract.setNickname(nickname);
   await tx.wait();
 }).addPositionalParam("nickname");
